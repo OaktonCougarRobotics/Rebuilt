bb1159bcc234d7419b64f722bf7d4edb8b98607c
Lamborghini Bugatti <yanismehenni2009@gmail.com>
2026-03-05 12:00:08 -0500

    merge drivetrain into main

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -69,7 +69,7 @@ public class RobotContainer {
         configureBindings();
   }
 
-  /**
+  /** 
    * Use this method to define your trigger->command mappings. Triggers can be created via the
    * {@link Trigger#Trigger(java.util.function.BooleanSupplier)} constructor with an arbitrary
    * predicate, or via the named factories in {@link
